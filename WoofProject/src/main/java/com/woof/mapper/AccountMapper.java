a75752fe84ef317b33dac8de82c6af2ec75ef75f
Back Hyeyeon <skwir21@gmail.com>
2024-01-08 17:30:30 +0900

    [Account add] VO + AccountService + AccountServiceImpl + Mapper

--- WoofProject/src/main/java/com/woof/mapper/AccountMapper.java
+++ WoofProject/src/main/java/com/woof/mapper/AccountMapper.java
@@ -1,5 +1,20 @@
 package com.woof.mapper;
 
-public interface AccountMapper {
+import java.util.List;
 
+import com.woof.domain.Account;
+
+public interface AccountMapper {
+	// 내정보
+	public Account getAccount(Account username) throws Exception;
+	// Account들의 모든정보 리스트
+	public List<Account> getAccountList() throws Exception;
+	//계정 등록
+	public void registerAccount(Account account) throws Exception;
+	//정보 수정
+	public void modifyAccount(Account account) throws Exception;
+	//계정 탈퇴
+	public void deleteAccount(Account username) throws Exception;
+	//admin이 개인계정을 찾기
+	public Account searcheAccount(Account searchKeyword) throws Exception;
 }
--- WoofProject/src/main/java/com/woof/service/AccountService.java
+++ WoofProject/src/main/java/com/woof/service/AccountService.java
@@ -1,5 +1,23 @@
 package com.woof.service;
 
-public interface AccountService {
+import java.util.List;
 
+import com.woof.domain.Account;
+
+
+public interface AccountService {
+	
+	public Account getAccount(Account username) throws Exception;
+	
+	public List<Account> getAccountList() throws Exception;
+	
+	public void registerAccount(Account account) throws Exception;
+	
+	public void modifyAccount(Account account) throws Exception;
+	
+	public void deleteAccount(Account username) throws Exception;
+	
+	public Account searcheAccount(Account searchKeyword) throws Exception;
+	
+	
 }
