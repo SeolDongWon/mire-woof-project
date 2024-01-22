aeedafc2cdfd31435ca3c776f73193ecb2ecb681
Woo Jin Han <wjjh23@gmail.com>
2024-01-22 11:47:22 +0900

    [Prepare re-merge to retrieve lost changes]

--- WoofProject/src/main/java/com/woof/controller/OrderHistoryController.java
+++ WoofProject/src/main/java/com/woof/controller/OrderHistoryController.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
